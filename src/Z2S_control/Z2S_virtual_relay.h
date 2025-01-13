cd257a8dcd20c83374a151834f12a62e98613823
lsroka76 <95999074+lsroka76@users.noreply.github.com>
2025-01-13 23:24:22 +0100

    cc

--- src/Z2S_control/Z2S_virtual_relay.h
+++ src/Z2S_control/Z2S_virtual_relay.h
@@ -16,7 +16,7 @@
  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
-#ifndef ZB_DEVICE_SCANNER
+#ifdef Z2S_GATEWAY
 
 #ifndef SRC_SUPLA_CONTROL_Z2S_VIRTUAL_RELAY_H_
 #define SRC_SUPLA_CONTROL_Z2S_VIRTUAL_RELAY_H_
@@ -47,4 +47,4 @@ class Z2S_VirtualRelay : public Relay {
 };  // namespace Supla
 
 #endif  // SRC_SUPLA_CONTROL_Z2S_VIRTUAL_RELAY_H_
-#endif  // #ifndef ZB_DEVICE_SCANNER
+#endif  // #ifdef Z2S_GATEWAY
