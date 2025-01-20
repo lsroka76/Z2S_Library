8d5cc03cf88ae9599c1f5e03db6487a057c02dd1
lsroka76 <95999074+lsroka76@users.noreply.github.com>
2025-01-20 22:10:25 +0100

    Update Z2S_devices_database.h

--- examples/Z2S_Gateway/Z2S_devices_database.h
+++ examples/Z2S_Gateway/Z2S_devices_database.h
@@ -81,7 +81,7 @@ static z2s_device_entity_t Z2S_DEVICES_LIST[] PROGMEM = {
   { .manufacturer_name = "_TZ3000_fllyghyj", .model_name = "TS0201", 
     .z2s_device_desc_id = Z2S_DEVICE_DESC_TEMPHUMIDITY_SENSOR, .z2s_device_endpoints_count = 1},
 
-  { .manufacturer_name = "_TZ3000_v1w2k9dd ", .model_name = "TS0201", 
+  { .manufacturer_name = "_TZ3000_v1w2k9dd", .model_name = "TS0201", 
     .z2s_device_desc_id = Z2S_DEVICE_DESC_TEMPHUMIDITY_SENSOR, .z2s_device_endpoints_count = 1},
 
   { .manufacturer_name = "_TZE204_upagmta9", .model_name = "TS0601", 
