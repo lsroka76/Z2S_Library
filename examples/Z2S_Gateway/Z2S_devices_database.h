ea1e65ea5edb7a71e0aeaa964f20d6ada6c6c394
lsroka76 <95999074+lsroka76@users.noreply.github.com>
2025-01-31 22:39:34 +0100

    update

--- examples/Z2S_Gateway/Z2S_devices_database.h
+++ examples/Z2S_Gateway/Z2S_devices_database.h
@@ -206,6 +206,9 @@ static z2s_device_entity_t Z2S_DEVICES_LIST[] PROGMEM = {
   { .manufacturer_name = "_TZ3000_w0qqde0g", .model_name = "TS011F", 
     .z2s_device_desc_id = Z2S_DEVICE_DESC_RELAY_ELECTRICITY_METER, .z2s_device_endpoints_count = 1},
 
+  { .manufacturer_name = "_TZ3000_ww6drja5", .model_name = "TS011F", 
+    .z2s_device_desc_id = Z2S_DEVICE_DESC_RELAY_ELECTRICITY_METER, .z2s_device_endpoints_count = 1},
+
   { .manufacturer_name = "_TZ3000_wkai4ga5", .model_name = "TS0044", 
     .z2s_device_desc_id = Z2S_DEVICE_DESC_ON_OFF, .z2s_device_endpoints_count = 4, 
     .z2s_device_endpoints = { { 1, Z2S_DEVICE_DESC_SWITCH_4X3 },
