a87a16452baf47aff742e5f893d421a9c3c96394
lsroka76 <95999074+lsroka76@users.noreply.github.com>
2025-03-24 19:09:30 +0100

    Update z2s_devices_database.h

--- examples/Z2S_Gateway/z2s_devices_database.h
+++ examples/Z2S_Gateway/z2s_devices_database.h
@@ -972,7 +972,7 @@ static z2s_device_entity_t Z2S_DEVICES_LIST[] PROGMEM = {
   { .manufacturer_name = "SONOFF", .model_name = "SNZB-02D", 
     .z2s_device_desc_id = Z2S_DEVICE_DESC_TEMPHUMIDITY_SENSOR, .z2s_device_endpoints_count = 1},
 
-  { .manufacturer_name = "SONOFF", .model_name = "SNZB-02P", 
+  { .manufacturer_name = "eWeLink", .model_name = "SNZB-02P", 
     .z2s_device_desc_id = Z2S_DEVICE_DESC_TEMPHUMIDITY_SENSOR, .z2s_device_endpoints_count = 1},
 
   { .manufacturer_name = "_TZ3000_bjawzodf", .model_name = "TY0201", 
