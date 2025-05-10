b755a4342f62e9800701f29a7865fe2936f48855
lsroka76 <95999074+lsroka76@users.noreply.github.com>
2025-05-10 17:21:10 +0200

    Update Z2S_Gateway.ino

--- examples/Z2S_Gateway/Z2S_Gateway.ino
+++ examples/Z2S_Gateway/Z2S_Gateway.ino
@@ -1430,8 +1430,6 @@ void loop() {
                 case Z2S_DEVICE_DESC_DIMMER_BULB:
                 case Z2S_DEVICE_DESC_TUYA_LED_DIMMER_F0_E0:
                 case Z2S_DEVICE_DESC_IKEA_WW_BULB:
-                case Z2S_DEVICE_DESC_TUYA_DIMMER_SWITCH:
-                case Z2S_DEVICE_DESC_TUYA_DIMMER_DOUBLE_SWITCH:
                 case Z2S_DEVICE_DESC_TUYA_RGB_LED_CONTROLLER_XY:
  {
                   if (zbGateway.sendAttributeRead(joined_device, ESP_ZB_ZCL_CLUSTER_ID_COLOR_CONTROL, ESP_ZB_ZCL_ATTR_COLOR_CONTROL_COLOR_CAPABILITIES_ID, true))
