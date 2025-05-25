2f1c6f03b1b327bcf208d98e48103925d231a8d5
lsroka76 <95999074+lsroka76@users.noreply.github.com>
2025-05-25 22:56:52 +0200

    Update z2s_devices_table.cpp

--- examples/Z2S_Gateway/z2s_devices_table.cpp
+++ examples/Z2S_Gateway/z2s_devices_table.cpp
@@ -1137,49 +1137,18 @@ void Z2S_onBatteryReceive(esp_zb_ieee_addr_t ieee_addr, uint16_t endpoint, uint1
   log_i("onBatteryReceive %x:%x:%x:%x:%x:%x:%x:%x, endpoint 0x%x, attribute id 0x%x, value %u", ieee_addr[7], ieee_addr[6], 
         ieee_addr[5], ieee_addr[4], ieee_addr[3], ieee_addr[2], ieee_addr[1], ieee_addr[0], endpoint, id, battery_remaining);
   
-  uint8_t zb_device_number_slot = Z2S_findZBDeviceTableSlot(ieee_addr);
-
-  if (zb_device_number_slot < 0xFF) {
-    if (z2s_zb_devices_table[zb_device_number_slot].user_data_flags & ZBD_USER_DATA_FLAG_DISABLE_BATTERY_MSG)
-      return;
-    if ((id == ESP_ZB_ZCL_ATTR_POWER_CONFIG_BATTERY_PERCENTAGE_REMAINING_ID) &&
-        (z2s_zb_devices_table[zb_device_number_slot].user_data_flags & ZBD_USER_DATA_FLAG_DISABLE_BATTERY_PERCENTAGE_MSG))
-      return;
-    if ((id == ESP_ZB_ZCL_ATTR_POWER_CONFIG_BATTERY_VOLTAGE_ID) &&
-        (z2s_zb_devices_table[zb_device_number_slot].user_data_flags & ZBD_USER_DATA_FLAG_DISABLE_BATTERY_VOLTAGE_MSG))
-      return;
-  }
-  Z2S_updateZBDeviceLastSeenMs(ieee_addr, millis());
-
-  switch (id) {
-    case ESP_ZB_ZCL_ATTR_POWER_CONFIG_BATTERY_PERCENTAGE_REMAINING_ID:
-      battery_remaining /= 2; break;
-    case ESP_ZB_ZCL_ATTR_POWER_CONFIG_BATTERY_VOLTAGE_ID:
-      battery_remaining = 100 - ((33 - battery_remaining)*20); break;
-  }
-
-  int16_t channel_number_slot = Z2S_findChannelNumberSlot(ieee_addr, endpoint, cluster, ALL_SUPLA_CHANNEL_TYPES, NO_CUSTOM_CMD_SID);
+  int16_t channel_number_slot = Z2S_findChannelNumberSlot(ieee_addr, -1, cluster, ALL_SUPLA_CHANNEL_TYPES, NO_CUSTOM_CMD_SID);
   
   if (channel_number_slot < 0) {
     log_i("No channel found for address %s", ieee_addr);
     return;
   }
-
-  if ((z2s_devices_table[channel_number_slot].model_id == Z2S_DEVICE_DESC_TEMPHUMIDITY_SENSOR_HUMIX10) &&
-      (id == ESP_ZB_ZCL_ATTR_POWER_CONFIG_BATTERY_VOLTAGE_ID)) {
-    log_i("Temporary fix for inaccurate voltage scaling"); //TODO add battery_voltage_max, battery_voltage_min for different models
-    return;
-  }
-
-  while (channel_number_slot >= 0)
-  {
-    auto element = Supla::Element::getElementByChannelNumber(z2s_devices_table[channel_number_slot].Supla_channel);
-    if (element) {
-      
-      element->getChannel()->setBatteryLevel(battery_remaining);
-      //element->getChannel()->setStateOnline()
-    }
-    channel_number_slot = Z2S_findChannelNumberNextSlot(channel_number_slot, ieee_addr, endpoint, cluster, ALL_SUPLA_CHANNEL_TYPES, NO_CUSTOM_CMD_SID);
+  
+  switch (id) {
+    case ESP_ZB_ZCL_ATTR_POWER_CONFIG_BATTERY_PERCENTAGE_REMAINING_ID:
+      updateSuplaBatteryLevel(channel_number_slot, ZBD_BATTERY_PERCENTAGE_MSG, battery_remaining); break;
+    case ESP_ZB_ZCL_ATTR_POWER_CONFIG_BATTERY_VOLTAGE_ID:
+      updateSuplaBatteryLevel(channel_number_slot, ZBD_BATTERY_VOLTAGE_MSG, battery_remaining); break;
   }
 }
 
@@ -2131,7 +2100,7 @@ bool z2s_add_action(char *action_name, uint8_t src_channel_id, uint16_t Supla_ac
 
 void updateSuplaBatteryLevel(int16_t channel_number_slot, uint8_t msg_id, uint32_t msg_value, signed char rssi) {
 
-  updateZBDeviceLastSeenMs(z2s_devices_table[channel_number_slot].ieee_addr, millis());
+  Z2S_updateZBDeviceLastSeenMs(z2s_devices_table[channel_number_slot].ieee_addr, millis());
   
   uint8_t zb_device_number_slot = Z2S_findZBDeviceTableSlot(z2s_devices_table[channel_number_slot].ieee_addr);
 
@@ -2140,10 +2109,19 @@ void updateSuplaBatteryLevel(int16_t channel_number_slot, uint8_t msg_id, uint32
       return;
   } 
   
-  auto element = Supla::Element::getElementByChannelNumber(z2s_devices_table[channel_number_slot].Supla_channel);
-    
-  if (element != nullptr) {
-    element->getChannel()->setBatteryLevel(value);
-    //element->getChannel()->setBridgeSignalStrength(Supla::rssiToSignalStrength(rssi));
+  /*if ((z2s_devices_table[channel_number_slot].model_id == Z2S_DEVICE_DESC_TEMPHUMIDITY_SENSOR_HUMIX10) &&
+      (id == ESP_ZB_ZCL_ATTR_POWER_CONFIG_BATTERY_VOLTAGE_ID)) {
+    log_i("Temporary fix for inaccurate voltage scaling"); //TODO add battery_voltage_max, battery_voltage_min for different models
+    return;
+  }*/
+
+  while (channel_number_slot >= 0)
+  {
+    auto element = Supla::Element::getElementByChannelNumber(z2s_devices_table[channel_number_slot].Supla_channel);
+    if (element) {
+      
+      element->getChannel()->setBatteryLevel(battery_remaining);
+    }
+    channel_number_slot = Z2S_findChannelNumberNextSlot(channel_number_slot, ieee_addr, -1, cluster, ALL_SUPLA_CHANNEL_TYPES, NO_CUSTOM_CMD_SID);
   }
 }
