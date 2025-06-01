bc984bb50f9f42b6640e51880b51b20dd02b956f
lsroka76 <95999074+lsroka76@users.noreply.github.com>
2025-06-01 22:51:35 +0200

    Update z2s_general_purpose_measurement.cpp

--- examples/Z2S_Gateway/z2s_general_purpose_measurement.cpp
+++ examples/Z2S_Gateway/z2s_general_purpose_measurement.cpp
@@ -13,16 +13,7 @@ uint64_t uipow10(uint8_t power) {
 
 uint64_t setU64Digits(int64_t value, uint8_t first_digit, uint8_t last_digit, uint64_t digits_to_insert) {
     
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-
     uint64_t result = (value/uipow10(last_digit));
-=======
-    uint32_t result = (value/uipow10(last_digit));
->>>>>>> Stashed changes
-=======
-    uint32_t result = (value/uipow10(last_digit));
->>>>>>> Stashed changes
     result = result * uipow10(last_digit );
     result += value % uipow10(first_digit-1);
     //printf("%lu, %lu\n\r", value, result);
