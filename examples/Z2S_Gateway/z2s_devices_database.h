c1a3b96349ea71cff0341aa67c9f9ae62f8bf924
lsroka76 <95999074+lsroka76@users.noreply.github.com>
2025-08-12 19:30:19 +0200

    Update z2s_devices_database.h

--- examples/Z2S_Gateway/z2s_devices_database.h
+++ examples/Z2S_Gateway/z2s_devices_database.h
@@ -2228,7 +2228,9 @@ static const z2s_device_entity_t Z2S_DEVICES_LIST[] PROGMEM = {
 
   { .manufacturer_name = "SONOFF", .model_name = "S60ZBTPF",
     .z2s_device_desc_id = Z2S_DEVICE_DESC_SONOFF_RELAY_ELECTRICITY_METER, .z2s_device_endpoints_count = 1},
-    
+  
+  { .manufacturer_name = "eWeLink", .model_name = "CK-BL702-SWP-01(7020)",
+    .z2s_device_desc_id = Z2S_DEVICE_DESC_SONOFF_RELAY_ELECTRICITY_METER, .z2s_device_endpoints_count = 1}
 };
 
 #endif
