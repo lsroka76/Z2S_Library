b80c56bb38e899e12cb1a8d8bec6c4b09cc5be09
lsroka76 <95999074+lsroka76@users.noreply.github.com>
2025-12-07 17:05:11 +0100

    Update z2s_web_gui.cpp

--- examples/Z2S_Gateway/z2s_web_gui.cpp
+++ examples/Z2S_Gateway/z2s_web_gui.cpp
@@ -3553,7 +3553,7 @@ void sprintfAction(z2s_channel_action_t &action) {
 	if (action.is_condition)
 		sprintf(general_purpose_gui_buffer, 
 						"<b>Action#:</b> <i>%d</i> <b>of</b> <i>%d</i><br><br>"
-						"<b>Action name:</b> <i>%s</i><br><br>"
+						"<b>Action name:</b> <i>%s (%s)</i><br><br>"
 						"<b>Condition:</b> <i>{%s}</i><br>"
 						"with <b>value(s) = </b> <i>%.2f, %.2f </i><br>"
 						"<b>for source channel:</b> <i>[%s]</i><br><br>"
@@ -3561,6 +3561,7 @@ void sprintfAction(z2s_channel_action_t &action) {
 						"on destination channel:</b> <i>[%s]</i>",
 						current_action_counter, Z2S_getActionsNumber(),
 						action.action_name, 
+						action.is_enabled ? "enabled" : "disabled",
 						getSuplaEventName(action.src_Supla_event, action.is_condition),
 						action.min_value,
 						action.max_value,
@@ -3572,13 +3573,14 @@ void sprintfAction(z2s_channel_action_t &action) {
 	else
 		sprintf(general_purpose_gui_buffer, 
 						"<b>Action#:</b> <i>%d</i> <b>of</b> <i>%d</i><br><br>"
-						"<b>Action name:</b> <i>%s</i><br><br>"
+						"<b>Action name:</b> <i>%s (%s)</i><br><br>"
 						"<b>Event:</b> <i>{%s}</i><br>"
 						"<b>from source channel:</b> <i>[%s]</i><br><br>"
 						"<b>Action:</b> <i>{%s}</i> <b><br>"
 						"on destination channel:</b> <i>[%s]</i>",
 						current_action_counter, Z2S_getActionsNumber(),
 						action.action_name, 
+						action.is_enabled ? "enabled" : "disabled",
 						getSuplaEventName(action.src_Supla_event, action.is_condition),
 						z2s_channels_table[Z2S_findTableSlotByChannelNumber(
 								action.src_Supla_channel)].Supla_channel_name, 
