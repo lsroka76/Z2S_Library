5b2297a582afd8a9249881262576a8a5ea35ecb6
lsroka76 <95999074+lsroka76@users.noreply.github.com>
2025-12-22 22:01:58 +0100

    Update z2s_devices_table.h

--- examples/Z2S_Gateway/z2s_devices_table.h
+++ examples/Z2S_Gateway/z2s_devices_table.h
@@ -780,15 +780,17 @@ public:
           Z2S_getZbDeviceLocalName(i), Z2S_getZbDeviceManufacturerName(i), 
           Z2S_getZbDeviceModelName(i));
 
-        strncpy(subdeviceDetails.SoftVer, "SOFTWARE VERSION",
-          SUPLA_SOFTVER_MAXSIZE);
+        strncpy(
+          subdeviceDetails.SoftVer, "SOFTWARE VERSION", SUPLA_SOFTVER_MAXSIZE);
 
-        snprintf(subdeviceDetails.ProductCode,
-          SUPLA_SUBDEVICE_PRODUCT_CODE_MAXSIZE, "%s (%s)",
-          "PRODUCT CODE", "HW VERSION");
+        snprintf(
+          subdeviceDetails.ProductCode, SUPLA_SUBDEVICE_PRODUCT_CODE_MAXSIZE, 
+          "%s (%s)", "PRODUCT CODE", "HW VERSION");
 
-        strncpy(subdeviceDetails.SerialNumber, "SERIAL",
-          SUPLA_SUBDEVICE_SERIAL_NUMBER_MAXSIZE);
+        char ieee_addr_str[24];
+        ieee_addr_to_str(ieee_addr_str, z2s_zb_devices_table[i].ieee_addr);
+        strncpy(
+          subdeviceDetails.SerialNumber, ieee_addr_str, SUPLA_SUBDEVICE_SERIAL_NUMBER_MAXSIZE);
 
         srpc->sendSubdeviceDetails(&subdeviceDetails);
         z2s_zb_devices_table[i].reserved_0 = 1;
