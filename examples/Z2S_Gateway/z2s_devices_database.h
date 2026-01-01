3e46ecc5889104e799378c0b2b4ea5f561c35f88
lsroka76 <95999074+lsroka76@users.noreply.github.com>
2026-01-01 00:54:40 +0000

    Update z2s_devices_database.h

--- examples/Z2S_Gateway/z2s_devices_database.h
+++ examples/Z2S_Gateway/z2s_devices_database.h
@@ -3042,6 +3042,11 @@ static const z2s_device_entity_t Z2S_DEVICES_LIST[] PROGMEM = {
 	  .z2s_device_desc_id = Z2S_DEVICE_DESC_TUYA_SMART_BUTTON_3F,
 	  .z2s_device_endpoints_count = 1},
 
+{	.manufacturer_name = "_TZ3000_b4awzgct", .model_name = "TS0041",
+    .z2s_device_uid = 14805,
+	  .z2s_device_desc_id = Z2S_DEVICE_DESC_TUYA_SMART_BUTTON_3F,
+	  .z2s_device_endpoints_count = 1},
+
   {	.manufacturer_name = "_TZ3000_8rppvwda", .model_name = "TS0041",
     .z2s_device_uid = 14900,
 	  .z2s_device_desc_id = Z2S_DEVICE_DESC_TUYA_SMART_BUTTON_3F,
