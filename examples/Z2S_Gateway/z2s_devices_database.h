30d1b8c6578090a727c62fbd7f26b142bad0beb1
lsroka76 <95999074+lsroka76@users.noreply.github.com>
2026-04-13 17:03:17 +0200

    Update z2s_devices_database.h

--- examples/Z2S_Gateway/z2s_devices_database.h
+++ examples/Z2S_Gateway/z2s_devices_database.h
@@ -2784,6 +2784,11 @@ static const z2s_device_entity_t Z2S_DEVICES_LIST[] PROGMEM = {
 	  .z2s_device_desc_id = Z2S_DEVICE_DESC_TUYA_SOIL_TEMPHUMIDITY_SENSOR,
 	  .z2s_device_endpoints_count = 1},
 
+ {	.manufacturer_name = "_TZE284_sgabhwa6", .model_name = "TS0601",
+    .z2s_device_uid = 3605,
+	  .z2s_device_desc_id = Z2S_DEVICE_DESC_TUYA_SOIL_TEMPHUMIDITY_SENSOR,
+	  .z2s_device_endpoints_count = 1},
+
   {	.manufacturer_name = "_TZE284_myd45weu", .model_name = "TS0601",
     .z2s_device_uid = 3700,
 	  .z2s_device_desc_id = Z2S_DEVICE_DESC_TUYA_SOIL_TEMPHUMIDITY_SENSOR_1,
