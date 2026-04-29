ff5a2b0a93ed28add1bf5ccf20990985dac31c70
lsroka76 <95999074+lsroka76@users.noreply.github.com>
2026-04-29 21:06:53 +0200

    1.5.26
    29/04/26

--- examples/Z2S_Gateway/z2s_devices_database.h
+++ examples/Z2S_Gateway/z2s_devices_database.h
@@ -3517,6 +3517,15 @@ static const z2s_device_entity_t Z2S_DEVICES_LIST[] PROGMEM = {
       Z2S_REPORTING_SET_DESC_BATTERY_PERCENTAGE_REPORTING_STANDARD, 
       Z2S_DEVICE_DESC_TUYA_IAS_ZONE_SENSOR}},
 
+  {	.manufacturer_name = "_TZ3040_6ygjfyll", .model_name = "TS0202",
+    .z2s_device_uid = 8205,
+	  .z2s_device_desc_id = Z2S_DEVICE_DESC_TUYA_IAS_ZONE_SENSOR,
+	  .z2s_device_endpoints_count = 1,
+    .z2s_device_endpoints = { 
+      1, Z2S_REPORTING_SET_FLAG_STANDARD, 
+      Z2S_REPORTING_SET_DESC_BATTERY_PERCENTAGE_REPORTING_STANDARD, 
+      Z2S_DEVICE_DESC_TUYA_IAS_ZONE_SENSOR}},
+
   {	.manufacturer_name = "_TZ3000_7d8yme6f", .model_name = "TS0203",
     .z2s_device_uid = 8300,
 	  .z2s_device_desc_id = Z2S_DEVICE_DESC_TUYA_IAS_ZONE_SENSOR,
--- examples/Z2S_Gateway/z2s_version_info.h
+++ examples/Z2S_Gateway/z2s_version_info.h
@@ -1,6 +1,6 @@
 #ifndef Z2S_VERSION_INFO_H_
 #define Z2S_VERSION_INFO_H_
 
-#define Z2S_VERSION "Z2S-1.5.25-28/04/26"
+#define Z2S_VERSION "Z2S-1.5.26-29/04/26"
 
 #endif  
