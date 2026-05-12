bacdefea319c866693b957971fc9cd844f749c13
lsroka76 <95999074+lsroka76@users.noreply.github.com>
2026-05-12 17:48:20 +0200

    1.5.31
    12/05/26

--- examples/Z2S_Gateway/z2s_devices_database.h
+++ examples/Z2S_Gateway/z2s_devices_database.h
@@ -2742,6 +2742,11 @@ static const z2s_device_entity_t Z2S_DEVICES_LIST[] PROGMEM = {
 	  .z2s_device_desc_id = Z2S_DEVICE_DESC_TUYA_TEMPHUMIDITY_SENSOR,
 	  .z2s_device_endpoints_count = 1 },
 
+  {	.manufacturer_name = "_TZ3000_yupc0pb7", .model_name = "TS0201",
+    .z2s_device_uid = 150,
+	  .z2s_device_desc_id = Z2S_DEVICE_DESC_TUYA_TEMPHUMIDITY_SENSOR,
+	  .z2s_device_endpoints_count = 1 },
+
   {	.manufacturer_name = "_TZ3000_dowj6gyi", .model_name = "TS0201",
     .z2s_device_uid = 200,
 	  .z2s_device_desc_id = Z2S_DEVICE_DESC_TUYA_TEMPHUMIDITY_SENSOR,
