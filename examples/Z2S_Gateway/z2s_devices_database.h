36726550223893913df13f3dbbb6f43da3115f53
lsroka76 <95999074+lsroka76@users.noreply.github.com>
2026-09-10 22:40:56 +0200

    Update z2s_devices_database.h

--- examples/Z2S_Gateway/z2s_devices_database.h
+++ examples/Z2S_Gateway/z2s_devices_database.h
@@ -3203,6 +3203,12 @@ static const z2s_device_entity_t Z2S_DEVICES_LIST[] PROGMEM = {
 	  .z2s_device_endpoints_count = 1
   },
 
+  {	.manufacturer_name = "SONOFF", .model_name = "SNZB-02B",
+    .z2s_device_uid = 2005,
+	  .z2s_device_desc_id = Z2S_DEVICE_DESC_TEMPHUMIDITY_SENSOR_POLL,
+	  .z2s_device_endpoints_count = 1
+  },
+
   {	.manufacturer_name = "SONOFF", .model_name = "SNZB-02DR2",
     .z2s_device_uid = 2050,
 	  .z2s_device_desc_id = Z2S_DEVICE_DESC_TEMPHUMIDITY_SENSOR_POLL,
