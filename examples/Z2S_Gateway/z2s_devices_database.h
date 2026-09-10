e1478b4c7fce2e9eb161a92df382a77c3f28c16a
lsroka76 <95999074+lsroka76@users.noreply.github.com>
2026-09-10 09:45:47 +0200

    1.5.165
    10/09/26

--- examples/Z2S_Gateway/z2s_devices_database.h
+++ examples/Z2S_Gateway/z2s_devices_database.h
@@ -3117,6 +3117,21 @@ static const z2s_device_entity_t Z2S_DEVICES_LIST[] PROGMEM = {
 	.z2s_device_desc_id = Z2S_DEVICE_DESC_TUYA_TEMPHUMIDITY_EF00_SENSOR,
 	.z2s_device_endpoints_count = 1},
 
+  {	.manufacturer_name = "_TZE200_yjjdcqsq", .model_name = "TS0601",
+    .z2s_device_uid = 910,
+	.z2s_device_desc_id = Z2S_DEVICE_DESC_TUYA_TEMPHUMIDITY_EF00_SENSOR,
+	.z2s_device_endpoints_count = 1},
+
+  {	.manufacturer_name = "_TZE204_yjjdcqsq", .model_name = "TS0601",
+    .z2s_device_uid = 915,
+	.z2s_device_desc_id = Z2S_DEVICE_DESC_TUYA_TEMPHUMIDITY_EF00_SENSOR,
+	.z2s_device_endpoints_count = 1},
+
+  {	.manufacturer_name = "_TZE284_yjjdcqsq", .model_name = "TS0601",
+    .z2s_device_uid = 920,
+	.z2s_device_desc_id = Z2S_DEVICE_DESC_TUYA_TEMPHUMIDITY_EF00_SENSOR,
+	.z2s_device_endpoints_count = 1},
+
   {	.manufacturer_name = "_TZE284_1wnh8bqp", .model_name = "TS0601",
     .z2s_device_uid = 1000,
 	.z2s_device_desc_id = Z2S_DEVICE_DESC_TUYA_TEMPHUMIDITY_EF00_SENSOR,
